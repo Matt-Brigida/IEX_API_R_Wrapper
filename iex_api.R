d94ce05af8c7fab299cb0b73039fc3af2cfdc514
Matt Brigida <mdbrigida@gmail.com>
2018-05-30 19:38:40 -0400

    added fins function

--- iex_api.R
+++ iex_api.R
@@ -6,12 +6,6 @@ library(httr)
 base_url <- "https://api.iextrading.com/"
 version <- "1.0/"
 
-## example GET
-
-test <- GET(paste0(base_url, version, "stock/aapl/company"))
-str(content(test, "parsed"))
-
-
 ## volume by venue for given ticker
 ven <- function(ticker){
     tmp <- GET(paste0(base_url, version, "stock/", ticker,"/volume-by-venue"))
@@ -22,7 +16,7 @@ ven <- function(ticker){
     return(tmp)
 }
 
-## one miunte prices over day for given ticker
+## one minute prices over day for given ticker
 one_day <- function(ticker){
     tmp <- GET(paste0(base_url, version, "stock/", ticker,"/chart/1d"))
     tmp <- content(tmp, "parsed")
@@ -32,10 +26,23 @@ one_day <- function(ticker){
     return(tmp)
 }
 
+## get all symbols (names and tickers), may change daily
 symbol_list <- function(){
     tmp <- GET(paste0(base_url, version, "/ref-data/symbols"))
     tmp <- content(tmp, "parsed")
     tmp <- do.call(rbind, tmp)
     tmp <- data.frame(tmp)
+    return(tmp)
+}
+
+## finacials for given ticker  --- work on this function, data returned is in a different structure. 
+fins <- function(ticker){
+    tmp <- GET(paste0(base_url, version, "stock/", ticker,"/financials"))
+    tmp <- content(tmp, "parsed")
+    tmp <- unlist(tmp)
+    names <- gsub("financials.", "", names(tmp))
+    iden <- c("", rep(c("", rep("Income Statement",8), rep("Balance Sheet", 7), rep("Cash Flow", 2)), 4))
+    tmp <- data.frame(tmp, rownames = names, fin_statement = iden)
+    names(tmp) <- c("data", "field", "Fin_Statement")
     return(tmp)
 }
