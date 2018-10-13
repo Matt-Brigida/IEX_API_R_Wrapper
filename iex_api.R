5f86c5f732fa3c3f0561ca5cb8f48fbf8380d88b
Matt Brigida <mdbrigida@gmail.com>
2018-10-13 12:21:36 -0400

    refactored

--- iex_api.R
+++ iex_api.R
@@ -6,34 +6,37 @@ library(httr)
 base_url <- "https://api.iextrading.com/"
 version <- "1.0/"
 
-## volume by venue for given ticker
-ven <- function(ticker){
-    tmp <- GET(paste0(base_url, version, "stock/", ticker,"/volume-by-venue"))
-    tmp <- content(tmp, "parsed")
-    tmp <- do.call(rbind, tmp)
-    tmp <- data.frame(tmp)
-    tmp$Ticker <- ticker
-    return(tmp)
+.datahandler <- function(tmpdata, ticker = ""){
+    tmpdata <- content(tmpdata, "parsed")
+    tmpdata <- do.call(rbind, tmpdata)
+    tmpdata <- data.frame(tmpdata)
+    tmpdata$Ticker <- ticker
+    return(tmpdata)
 }
 
+## volume by venue for given ticker
+ven <- function(ticker){
+    tmpdata <- GET(paste0(base_url, version, "stock/", ticker,"/volume-by-venue"))
+    .datahandler(tmpdata, ticker)
+}
+
+book <- function(ticker){
+    tmpdata <- GET(paste0(base_url, version, "stock/", ticker,"/book"))
+    .datahandler(tmpdata, ticker)
+}
+  
 ## one minute prices over day for given ticker
 one_day <- function(ticker){
-    tmp <- GET(paste0(base_url, version, "stock/", ticker,"/chart/1d"))
-    tmp <- content(tmp, "parsed")
-    tmp <- do.call(rbind, tmp)
-    tmp <- data.frame(tmp)
-    tmp$Ticker <- ticker
-    return(tmp)
+    tmpdata <- GET(paste0(base_url, version, "stock/", ticker,"/chart/1d"))
+    .datahandler(tmpdata, ticker)
 }
 
 ## get all symbols (names and tickers), may change daily
 symbol_list <- function(){
-    tmp <- GET(paste0(base_url, version, "/ref-data/symbols"))
-    tmp <- content(tmp, "parsed")
-    tmp <- do.call(rbind, tmp)
-    tmp <- data.frame(tmp)
-    return(tmp)
+    tmpdata <- GET(paste0(base_url, version, "/ref-data/symbols"))
+    .datahandler(tmpdata)
 }
+
 
 ## finacials for given ticker  --- work on this function, data returned is in a different structure. 
 fins <- function(ticker){
@@ -41,10 +44,7 @@ fins <- function(ticker){
     tmp <- content(tmp, "parsed")
     tmp <- unlist(tmp)
     names <- gsub("financials.", "", names(tmp))
-    ## iden <- c("", rep(c("", rep("Income Statement",8), rep("Balance Sheet", 7), rep("Cash Flow", 2)), 4))
-    ## tmp <- data.frame(tmp, rownames = names, fin_statement = iden)
     tmp <- data.frame(tmp, rownames = names)
-    ## names(tmp) <- c("data", "field", "Fin_Statement")
     names(tmp) <- c("data", "field")
     return(tmp)
 }
